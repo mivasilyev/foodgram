80635e14842cd21e0c4083ecd7bbdca29131e67b
Mikhail Vasilyev <milhail-vasilyev@yandex.ru>
2025-04-22 20:44:06 +0800

    Fourteen

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, path
 
 from api.views import short_link_redirect
 
@@ -7,6 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('s/<str:short_link>/', short_link_redirect)
-    # re_path(r'^s/(?P<short_link>[а-яёА-ЯЁ\-]+)/$', short_link_redirect),
-    # re_path(r'^s/(?P<short_link>[a-zA-Z\-]+)/$', short_link_redirect),
 ]
